a543e00d6ca0e9255846f5bf0c96237c1500f960
kalaiyarasi ganeshalingam <ashakalai31@gmail.com>
2016-01-07 17:57:03 +0530

    6th commit

---
+++ process_login.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+ 
+ 
+sec_session_start(); // Our custom secure way of starting a PHP session.
+ 
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // The hashed password.
+ 
+    if (login($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: admin/protected_page.php');
+    
+    }
+     elseif (login1($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: user1/protected_page.php');
+    
+    }
+     elseif (login2($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: user2/protected_page.php');
+    
+    }
+     elseif (login3($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: user3/protected_page.php.php');
+    
+    }else {
+        // Login failed 
+        header('Location: index.php?error=1');
+    }
+} else {
+    // The correct POST variables were not sent to this page. 
+    echo 'Invalid Request';
+}
+?>
