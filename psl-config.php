36580be9594066bf945d891d7515e240aec812d5
kalaiyarasi ganeshalingam <ashakalai31@gmail.com>
2016-01-07 18:08:31 +0530

    12th commit

---
+++ psl-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "root");    // The database username. 
+define("PASSWORD", "");    // The database password. 
+define("DATABASE", "db_documentation");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+
+?>
